8e059ac22ab561b927bb22827876e6242ee67920
Nithichai <rockon23146@gmail.com>
2019-04-14 19:58:42 +0700

    Update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ microgear.on('connected', function() {
   setInterval(function() {
     var device = queue.splice(-1, 1)
     if (device.length > 0) {
-      microgear.setAlias(device[0]);
+      microgear.setAlias("bhcd-" + device[0]);
       microgear.chat(device[0], 'ACK')
     }
   }, 50);
